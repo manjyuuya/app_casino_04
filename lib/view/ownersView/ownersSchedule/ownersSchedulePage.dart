f03e78b716a020a4af8a0931aef3ffc7b8805d5f
WL-Yahata <106352826+WL-Yahata@users.noreply.github.com>
2022-12-23 13:26:58 +0900

    Update ownersSchedulePage.dart

--- lib/view/ownersView/ownersSchedule/ownersSchedulePage.dart
+++ lib/view/ownersView/ownersSchedule/ownersSchedulePage.dart
@@ -1,5 +1,9 @@
+import 'dart:math';
 import 'package:app_casino_03/view/ownersView/ownersSchedule/scheTournamentList.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:intl/intl.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class OwnersSchedulePage extends StatefulWidget {
@@ -10,19 +14,83 @@ class OwnersSchedulePage extends StatefulWidget {
 }
 
 class _OwnersSchedulePageState extends State<OwnersSchedulePage> {
+  List<Color> _colorCollection = <Color>[];
+  MeetingDataSource? events;
+  final List<String> options = <String>['Add', 'Delete', 'Update'];
+  final fireStoreReference = FirebaseFirestore.instance;
+  bool isInitialLoaded = false;
+
+  @override
+  void initState() {
+    _initializeEventColor();
+    getDataFromFireStore().then((results) {
+      SchedulerBinding.instance!.addPostFrameCallback((timeStamp) {
+        setState(() {});
+      });
+    });
+    super.initState();
+  }
+
+  Future<void> getDataFromFireStore() async {
+    var snapShotsValue = await FirebaseFirestore.instance
+        .collection("CalendarAppointmentCollection")
+        .get();
+
+    final Random random = new Random();
+    List<Meeting> list = snapShotsValue.docs
+        .map((e) => Meeting(
+            eventName: e.data()['Subject'],
+            from:
+                DateFormat('dd/MM/yyyy HH:mm:ss').parse(e.data()['StartTime']),
+            to: DateFormat('dd/MM/yyyy HH:mm:ss').parse(e.data()['EndTime']),
+            background: _colorCollection[random.nextInt(9)],
+            isAllDay: false))
+        .toList();
+    setState(() {
+      events = MeetingDataSource(list);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    isInitialLoaded = true;
+    return Scaffold(
         appBar: AppBar(
-          centerTitle: true,
-          title: const Text(
-            'schedule',
-            style: TextStyle(color: Colors.black54),
-          ),
-          backgroundColor: Colors.white,
-          elevation: 1,
-        ),
+            leading: PopupMenuButton<String>(
+          icon: Icon(Icons.settings),
+          itemBuilder: (BuildContext context) => options.map((String choice) {
+            return PopupMenuItem<String>(
+              value: choice,
+              child: Text(choice),
+            );
+          }).toList(),
+          onSelected: (String value) {
+            if (value == 'Add') {
+              fireStoreReference
+                  .collection("CalendarAppointmentCollection")
+                  .doc("1")
+                  .set({
+                'Subject': 'Mastering Flutter',
+                'StartTime': '07/04/2020 08:00:00',
+                'EndTime': '07/04/2020 09:00:00'
+              });
+            } else if (value == "Delete") {
+              try {
+                fireStoreReference
+                    .collection('CalendarAppointmentCollection')
+                    .doc('1')
+                    .delete();
+              } catch (e) {}
+            } else if (value == "Update") {
+              try {
+                fireStoreReference
+                    .collection('CalendarAppointmentCollection')
+                    .doc('1')
+                    .update({'Subject': 'Meeting'});
+              } catch (e) {}
+            }
+          },
+        )),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             Navigator.push(
@@ -35,13 +103,84 @@ class _OwnersSchedulePageState extends State<OwnersSchedulePage> {
         ),
         body: SfCalendar(
           view: CalendarView.month,
-          monthViewSettings: const MonthViewSettings(
-            appointmentDisplayMode: MonthAppointmentDisplayMode.appointment,
+          initialDisplayDate: DateTime(2020, 4, 5, 9, 0, 0),
+          dataSource: events,
+          monthViewSettings: MonthViewSettings(
             showAgenda: true,
-            showTrailingAndLeadingDates: false,
           ),
-        ),
-      ),
-    );
+        ));
+  }
+
+  void _initializeEventColor() {
+    _colorCollection.add(const Color(0xFF0F8644));
+    _colorCollection.add(const Color(0xFF8B1FA9));
+    _colorCollection.add(const Color(0xFFD20100));
+    _colorCollection.add(const Color(0xFFFC571D));
+    _colorCollection.add(const Color(0xFF36B37B));
+    _colorCollection.add(const Color(0xFF01A1EF));
+    _colorCollection.add(const Color(0xFF3D4FB5));
+    _colorCollection.add(const Color(0xFFE47C73));
+    _colorCollection.add(const Color(0xFF636363));
+    _colorCollection.add(const Color(0xFF0A8043));
+  }
+}
+
+class MeetingDataSource extends CalendarDataSource {
+  MeetingDataSource(List<Meeting> source) {
+    appointments = source;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    return appointments![index].from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return appointments![index].to;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return appointments![index].isAllDay;
+  }
+
+  @override
+  String getSubject(int index) {
+    return appointments![index].eventName;
+  }
+
+  @override
+  Color getColor(int index) {
+    return appointments![index].background;
+  }
+}
+
+class Meeting {
+  String? eventName;
+  DateTime? from;
+  DateTime? to;
+  Color? background;
+  bool? isAllDay;
+  String? key;
+
+  Meeting(
+      {this.eventName,
+      this.from,
+      this.to,
+      this.background,
+      this.isAllDay,
+      this.key});
+
+  static Meeting fromFireBaseSnapShotData(dynamic element, Color color) {
+    return Meeting(
+        eventName: element.doc.data()!['Subject'],
+        from: DateFormat('dd/MM/yyyy HH:mm:ss')
+            .parse(element.doc.data()!['StartTime']),
+        to: DateFormat('dd/MM/yyyy HH:mm:ss')
+            .parse(element.doc.data()!['EndTime']),
+        background: color,
+        isAllDay: false,
+        key: element.doc.id);
   }
 }
